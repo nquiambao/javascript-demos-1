5a8a50612cf0405f9f9a059c3bf8761d78ad851e
Nica Quiambao <n.quiambao@hotmail.com>
2021-01-11 09:40:23 -0700

    worked on exercise

--- w1/dd_exercise/js/main.js
+++ w1/dd_exercise/js/main.js
@@ -1,4 +1,14 @@
+let h1 = document.querySelector('h1');
+console.log('h1.innerHTML = ' + h1.innerHTML);
+
+let img = document.querySelector('img');
+console.log(img.innerHTML);
+
+let p = document.querySelector('p');
+console.log('p.innerHTML = ' + p.innerHTML);
+
 let intro = document.querySelector('h2.intro');
 console.log('h2.intro.innerHTML = ' + intro.innerHTML);
+
 intro.innerHTML = 'Introduction';
 console.log('h2.intro.innerHTML = ' + intro.innerHTML);
